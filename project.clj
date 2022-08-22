9db22fcbb62d519af936ea429ddf232ea6b10923
fmnoise <fmnoise@gmail.com>
2022-08-22 21:34:09 +0200

    VERSION 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.fmnoise/template "0.1.0-SNAPSHOT"
+(defproject org.clojars.fmnoise/template "0.1.0"
   :description "Yet another templating library"
   :license {:name "EPL-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
